b54c63ebf89a5ac46f7fb1a0034f491290600fb6
myoponeponechit <myoponeponechit@gmail.com>
2023-08-06 01:27:43 +0630

    Second Upload

---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $table = 'posts';
+    protected $fillable = [
+        'title',
+        'photo',
+        'description',
+        'created_at',
+        'updated_at',
+        'category_id',
+        'user_id'
+    ];
+}
